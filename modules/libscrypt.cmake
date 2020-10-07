5959d194da788a8715c29a2319d96ec78dd67c01
Ivan Shynkarenka <chronoxor@gmail.com>
2020-10-07 19:20:25 +0300

    build

--- modules/libscrypt.cmake
+++ modules/libscrypt.cmake
@@ -11,17 +11,19 @@ if(NOT TARGET libscrypt)
     "libscrypt/sha256.c"
     "libscrypt/slowequals.c"
   )
+  add_library(libscrypt ${SOURCE_FILES})
   if(MSVC)
     # C4013: 'function' undefined; assuming extern returning int
     # C4131: 'function' : uses old-style declarator
     # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
     # C4245: 'conversion' : conversion from 'type1' to 'type2', signed/unsigned mismatch
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4013 /wd4131 /wd4244 /wd4245")
+    set_target_properties(libscrypt PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4013 /wd4131 /wd4244 /wd4245")
+  else()
+    set_target_properties(libscrypt PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
   endif()
-  add_library(libscrypt ${SOURCE_FILES})
   target_include_directories(libscrypt PUBLIC "libscrypt")
 
   # Module folder
-  set_target_properties(libscrypt PROPERTIES FOLDER modules/libscrypt)
+  set_target_properties(libscrypt PROPERTIES FOLDER "modules/libscrypt")
 
 endif()

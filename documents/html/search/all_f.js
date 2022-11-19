64e2b2ce86c100a906c8e108b63e15bab6e9e36b
Ivan Shynkarenka <chronoxor@gmail.com>
2022-11-20 03:22:04 +0400

    Migrate to OpenSSL 3.0.x

--- documents/html/search/all_f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['t_71',['t',['../class_cpp_security_1_1_argon2d_password_hashing.html#aa2f4e54e3af24f2b9606d93af8a6dc1b',1,'CppSecurity::Argon2dPasswordHashing']]]
-];
--- source/security/password_hashing_pbkdf2.cpp
+++ source/security/password_hashing_pbkdf2.cpp
@@ -8,7 +8,7 @@
 
 #include "security/password_hashing_pbkdf2.h"
 
-#include <fastpbkdf2.h>
+#include <openssl/evp.h>
 
 namespace CppSecurity {
 
@@ -24,38 +24,44 @@ PBKDF2PasswordHashing::PBKDF2PasswordHashing(size_t hash_length, size_t salt_len
 std::string PBKDF2PasswordHashing::GenerateHash(std::string_view password, std::string_view salt) const
 {
     // Generate the strong password hash
+    const EVP_MD* md = nullptr;
     std::string hash(hash_length(), 0);
     switch (algorithm())
     {
         case PBKDF2::HMAC_SHA1:
-            fastpbkdf2_hmac_sha1((const uint8_t*)password.data(), password.size(), (const uint8_t*)salt.data(), salt.size(), (uint32_t)iterations(), (uint8_t*)hash.data(), hash.size());
+            md = EVP_sha1();
             break;
         case PBKDF2::HMAC_SHA256:
-            fastpbkdf2_hmac_sha256((const uint8_t*)password.data(), password.size(), (const uint8_t*)salt.data(), salt.size(), (uint32_t)iterations(), (uint8_t*)hash.data(), hash.size());
+            md = EVP_sha256();
             break;
         case PBKDF2::HMAC_SHA512:
-            fastpbkdf2_hmac_sha512((const uint8_t*)password.data(), password.size(), (const uint8_t*)salt.data(), salt.size(), (uint32_t)iterations(), (uint8_t*)hash.data(), hash.size());
+            md = EVP_sha512();
             break;
     }
+    if (PKCS5_PBKDF2_HMAC((const char*)password.data(), (int)password.size(), (const uint8_t*)salt.data(), (int)salt.size(), (int)iterations(), md, (int)hash.size(), (uint8_t*)hash.data()) == 0)
+        throwex CppCommon::SecurityException("Cannot generate 'PBKDF2' hash!");
     return hash;
 }
 
 bool PBKDF2PasswordHashing::Validate(std::string_view password, std::string_view hash, std::string_view salt) const
 {
     // Calculate the digest for the given password and salt
+    const EVP_MD* md = nullptr;
     std::string digest(hash.size(), 0);
     switch (algorithm())
     {
         case PBKDF2::HMAC_SHA1:
-            fastpbkdf2_hmac_sha1((const uint8_t*)password.data(), password.size(), (const uint8_t*)salt.data(), salt.size(), (uint32_t)iterations(), (uint8_t*)digest.data(), digest.size());
+            md = EVP_sha1();
             break;
         case PBKDF2::HMAC_SHA256:
-            fastpbkdf2_hmac_sha256((const uint8_t*)password.data(), password.size(), (const uint8_t*)salt.data(), salt.size(), (uint32_t)iterations(), (uint8_t*)digest.data(), digest.size());
+            md = EVP_sha256();
             break;
         case PBKDF2::HMAC_SHA512:
-            fastpbkdf2_hmac_sha512((const uint8_t*)password.data(), password.size(), (const uint8_t*)salt.data(), salt.size(), (uint32_t)iterations(), (uint8_t*)digest.data(), digest.size());
+            md = EVP_sha512();
             break;
     }
+    if (PKCS5_PBKDF2_HMAC((const char*)password.data(), (int)password.size(), (const uint8_t*)salt.data(), (int)salt.size(), (int)iterations(), md, (int)digest.size(), (uint8_t*)digest.data()) == 0)
+        throwex CppCommon::SecurityException("Cannot calculate 'PBKDF2' hash!");
 
     // Compare the digest with the given hash
     return (digest == hash);

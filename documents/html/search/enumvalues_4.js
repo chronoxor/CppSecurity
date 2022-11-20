c59f81a16f49c65aa3600f57d6573db47822ae75
Ivan Shynkarenka <chronoxor@gmail.com>
2022-11-20 05:53:26 +0400

    Migrate to OpenSSL 3.0.x

--- documents/html/search/enumvalues_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['symbols_181',['symbols',['../namespace_cpp_security.html#ac4bec50eb73ce16dfb4ad4948c74af3ea5503577415fc1d8d6b3818212a1745bc',1,'CppSecurity']]]
-];
--- documents/html/search/typedefs_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['other_170',['other',['../struct_cpp_security_1_1_password_allocator_1_1rebind.html#a10451da14f0ecd06163c4b53b1275b41',1,'CppSecurity::PasswordAllocator::rebind']]]
-];

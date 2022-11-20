8843bfec6da5581be2d01070ad89af90367c60f7
Ivan Shynkarenka <chronoxor@gmail.com>
2022-11-20 05:40:55 +0400

    Migrate to OpenSSL 3.0.x

--- documents/html/search/functions_d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['r_151',['r',['../class_cpp_security_1_1_scrypt_password_hashing.html#a377605bedb82a4c0058ac74af7f81ff3',1,'CppSecurity::ScryptPasswordHashing']]]
-];
--- documents/html/search/typedefs_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['value_5ftype_172',['value_type',['../class_cpp_security_1_1_password_allocator.html#a5d90d8dfc567052cfb0f5432ec7ef64f',1,'CppSecurity::PasswordAllocator']]]
-];

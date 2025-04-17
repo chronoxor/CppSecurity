966a1834402459961a109f8f35b0095b7e312531
chronoxor <chronoxor@gmail.com>
2025-04-17 04:44:36 +0200

    Fix Argon2 build for ARM64

--- modules/Argon2.cmake
+++ modules/Argon2.cmake
@@ -4,8 +4,13 @@ if(NOT TARGET Argon2)
   find_package(Threads REQUIRED)
 
   # Module library
-  file(GLOB SOURCE_FILES "Argon2/src/argon2.c" "Argon2/src/core.c" "Argon2/src/encoding.c" "Argon2/src/opt.c" "Argon2/src/thread.c" "Argon2/src/blake2/blake2b.c")
-  add_library(Argon2 ${SOURCE_FILES})
+  file(GLOB SOURCE_FILES "Argon2/src/argon2.c" "Argon2/src/core.c" "Argon2/src/encoding.c" "Argon2/src/thread.c" "Argon2/src/blake2/blake2b.c")
+  if(CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
+    file(GLOB SOURCE_PALTFORM_FILES "Argon2/src/ref.c")
+  else()
+    file(GLOB SOURCE_PALTFORM_FILES "Argon2/src/opt.c")
+  endif()
+  add_library(Argon2 ${SOURCE_FILES} ${SOURCE_PALTFORM_FILES})
   if(MSVC)
     # C4100: 'identifier' : unreferenced formal parameter
     # C4204: nonstandard extension used : non-constant aggregate initializer

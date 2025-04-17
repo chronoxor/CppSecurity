a37f7f4e51d83e3c550c266d74b4b2f4e11a4efe
chronoxor <chronoxor@gmail.com>
2025-04-17 02:59:12 +0200

    Fix compilation errors

--- include/security/version.h
+++ include/security/version.h
@@ -28,7 +28,7 @@ GitHub: https://github.com/chronoxor/CppSecurity
 namespace CppSecurity {
 
 //! Project version
-const char version[] = "1.1.1.0";
+const char version[] = "1.1.2.0";
 
 } // namespace CppSecurity
 

7577077952991fc4f0bc33765d5a8fd695dfab20
chronoxor <chronoxor@users.noreply.github.com>
2026-02-27 20:40:50 +0000

    Update generated documentation

--- search/classes_4.js
+++ search/classes_4.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['password_0',['password',['../classstd_1_1password.html',1,'std']]],
   ['passwordallocator_1',['PasswordAllocator',['../class_cpp_security_1_1_password_allocator.html',1,'CppSecurity']]],
-  ['passwordgenerator_2',['PasswordGenerator',['../class_cpp_security_1_1_password_generator.html',1,'CppSecurity']]],
-  ['passwordhashing_3',['PasswordHashing',['../class_cpp_security_1_1_password_hashing.html',1,'CppSecurity']]],
-  ['pbkdf2passwordhashing_4',['PBKDF2PasswordHashing',['../class_cpp_security_1_1_p_b_k_d_f2_password_hashing.html',1,'CppSecurity']]]
+  ['passwordallocator_3c_20u_20_3e_2',['PasswordAllocator&lt; U &gt;',['../class_cpp_security_1_1_password_allocator.html',1,'CppSecurity']]],
+  ['passwordgenerator_3',['PasswordGenerator',['../class_cpp_security_1_1_password_generator.html',1,'CppSecurity']]],
+  ['passwordhashing_4',['PasswordHashing',['../class_cpp_security_1_1_password_hashing.html',1,'CppSecurity']]],
+  ['pbkdf2passwordhashing_5',['PBKDF2PasswordHashing',['../class_cpp_security_1_1_p_b_k_d_f2_password_hashing.html',1,'CppSecurity']]]
 ];
